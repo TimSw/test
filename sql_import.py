5d90f2dc51e95bfeca5b799c0691a73fa95230bf
timswinnen <Tim.Swinnen@iCloud.com>
2021-02-25 22:50:28 +0100

    test sql export import

--- sql_import.py
+++ sql_import.py
@@ -9,9 +9,24 @@ cur = con.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS testtable
             (data_1 REAL, data_2 REAL)''')
 
+# Select data from table
+cur.execute("SELECT data_1 FROM testtable")
+data = cur.fetchone()[0]
+print(data)
+
 # Select all data ordered
-for row in cur.execute("SELECT * FROM testtable ORDER BY data_1"):
-    print(row)
+# for row in cur.execute("SELECT * FROM testtable ORDER BY data_1"):
+#     print(row)
+
+# Initialise data lists
+# data_1 = []
+# data_2 = []
+# Select all data and append list
+# cur.execute("SELECT * FROM testtable")
+# data = cur.fetchall()
+# for row in data:
+#     data_1.append(row[0])
+#     data_2.append(row[1])
 
 print("Data gelezen")
 

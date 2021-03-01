0debe8fd024d03e12208ffdba552f39a16d343c6
timswinnen <Tim.Swinnen@iCloud.com>
2021-03-01 12:06:58 +0100

    GPIO sql threading

---
+++ gpio/RPiGPIO_sql_threading.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+import time
+import datetime
+import RPi.GPIO
+import sqlite3
+import threading
+
+# Initialise RPi.GPIO
+uit = RPi.GPIO.HIGH
+aan = RPi.GPIO.LOW
+outputList = (29, 31, 33, 35)
+RPi.GPIO.setmode(RPi.GPIO.BOARD)
+RPi.GPIO.setup(outputList, RPi.GPIO.OUT, initial=uit)
+
+# Initialise data
+nu = datetime.datetime.now()
+print("Het is", nu.hour, "uur")
+print("en", nu.minute, "minuten")
+# export_startuur = 0
+# export_startmin = 0
+
+
+def get_input():
+    # Initialise sqlite
+    con = sqlite3.connect('../testdata.db')
+    cur = con.cursor()
+
+    export_startuur = input("Please enter start hour:\n")
+    print(f'You entered {export_startuur} uur')
+    export_startmin = input("Please enter start minute:\n")
+    print(f'You entered {export_startmin} minuten')
+
+    # Create table
+    cur.execute('''CREATE TABLE IF NOT EXISTS testtable
+                (data_1 INT, data_2 INT)''')
+
+    # Export
+    # Update a row of data
+    cur.execute("UPDATE testtable SET data_1 = ?, data_2 = ?",
+                (export_startuur, export_startmin))
+    print("Data verzonden")
+
+    # Save (commit) the changes
+    con.commit()
+
+    # Close sql connection
+    con.close()
+
+
+def process_imput():
+    # Initialise sqlite
+    con = sqlite3.connect('../testdata.db')
+    cur = con.cursor()
+
+    # Select data from table
+    cur.execute("SELECT * FROM testtable")
+    data = cur.fetchone()
+    print(data)
+    import_startuur = data[0]
+    import_starmin = data[1]
+    print(import_startuur)
+    print(import_starmin)
+
+    # Process
+    if import_startuur == nu.hour and import_starmin > nu.minute:
+        RPi.GPIO.output(29, aan)
+        print("1 AAN")
+    else:
+        RPi.GPIO.output(29, uit)
+        print("1 UIT")
+
+    # Close sql connection
+    con.close()
+
+
+if __name__ == "__main__":
+    print("Voor creëren thread 1")
+    t1 = threading.Thread(target=get_input)
+    print("Voor starten thread 1")
+    t1.start()
+    #get_input()
+    process_imput()
+
+    # Cleanup
+    RPi.GPIO.cleanup()
+    print("CLEANUP")

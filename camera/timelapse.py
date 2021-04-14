d9f604f050133452062624dfe7827841f35a4b2c
timswinnen <Tim.Swinnen@iCloud.com>
2021-04-14 11:28:24 +0200

    images folder

--- camera/timelapse.py
+++ camera/timelapse.py
@@ -5,6 +5,6 @@ from picamera import PiCamera
 camera = PiCamera()
 camera.start_preview()
 sleep(2)
-for filename in camera.capture_continuous('img{counter:03d}.jpg'):
-    print('Captured %s' % filename)
+for filename in camera.capture_continuous("images/img{counter:03d}.jpg"):
+    print("Captured %s" % filename)
     sleep(300) # wait 5 minutes

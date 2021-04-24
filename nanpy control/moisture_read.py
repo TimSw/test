9a750401a600312553efc04dbc5d03bef2e8d0e4
timswinnen <Tim.Swinnen@iCloud.com>
2021-04-24 13:43:53 +0200

    nanpy read moisture level

--- nanpy control/moisture_read.py
+++ nanpy control/moisture_read.py
@@ -8,9 +8,9 @@ import time
 
 # Logging
 # Open logging configuration
-logging.config.fileConfig("logging.conf")
+#logging.config.fileConfig("logging.conf")
 # create logger
-logger = logging.getLogger("root")
+#logger = logging.getLogger("root")
 
 # Define data.db directory
 data_db = "/home/pi/growPiProject/data.db"
@@ -33,13 +33,13 @@ A0 = 14     # D14 - A0 - PC0 - ADC[0]
 # Read moisture level
 time.sleep(2)
 moisture = arduino.analogRead(A0)  # Analog input
-logger.debug("Moisture level = %s", moisture)
+#logger.debug("Moisture level = %s", moisture)
 
 # Make timestamp
 now = time.time()
 
 # Insert a row of data
-cur.execute("INSERT INTO temperature VALUES (?, ?)",
+cur.execute("INSERT INTO moisture VALUES (?, ?)",
             (now, moisture))
 
 # Save (commit) the changes

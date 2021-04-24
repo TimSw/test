ed0061e54f168bb25b0e2c8a0d9ec7bcbfe090b1
timswinnen <Tim.Swinnen@iCloud.com>
2021-04-24 11:01:18 +0200

    nanpy

--- nanpy control/ardiuino analog in.py
+++ nanpy control/ardiuino analog in.py
@@ -6,13 +6,34 @@ import nanpy
 # from nanpy import (ArduinoApi, SerialManager)
 # from time import sleep
 
-connection = nanpy.SerialManager()
+connection = nanpy.SerialManager(device="/dev/ttyUSB0")
 a = nanpy.ArduinoApi(connection=connection)
 
-A1 = 14  # Pot on A0 - Anaglog input
+D0 = 0      # D0/RX - PD0
+D1 = 1      # D1/TX - PD1
+D2 = 2      # D2 - PD2
+D3 = 3      # D3 - PD3
+D4 = 4      # D4 - PD4
+D5 = 5      # D5 - PD5
+D6 = 6      # D6 - PD6
+D7 = 7      # D7 - PD7
+
+D8 = 8      # D8 - PB0
+D9 = 9      # D9 - PB1
+D10 = 10    # D10 - PB2
+D11 = 11    # D11 - PB3
+D12 = 12    # D12 - PB4
+D13 = 13    # D13 - PB5
+
+A0 = 14     # D14 - A0 - PC0 - ADC[0]
+A1 = 15     # D15 - A1 - PC1 - ADC[1]
+A2 = 16     # D16 - A2 - PC2 - ADC[2]
+A3 = 17     # D17 - A3 - PC3 - ADC[3]
+A4 = 18     # D18 - A4 - PC4 - ADC[4] - SDA
+A5 = 19     # D19 - A5 - PC5 - ADC[5] - SCL
 
 print("Turn the pot - Analogue input - 10 bit")
 for i in range(0, 40):
-    val = a.analogRead(A1)  # Analog input
+    val = a.analogRead(A0)  # Analog input
     print(val)
     time.sleep(0.3)

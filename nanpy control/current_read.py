4db2c400010e75ab67cc6bb7d8f659fff23bffce
timswinnen <Tim.Swinnen@iCloud.com>
2021-05-15 10:20:02 +0200

    current sensor

---
+++ nanpy control/current_read.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import nanpy
+import time
+
+# Initialise arduino connection
+connection = nanpy.SerialManager(device="/dev/ttyUSB0")
+arduino = nanpy.ArduinoApi(connection=connection)
+
+# Define pin
+A0 = 14     # D14 - A0 - PC0 - ADC[0]
+
+moisture = 0.00000
+
+# Read moisture level
+time.sleep(2)
+moisture = arduino.analogRead(A0)  # Analog input
